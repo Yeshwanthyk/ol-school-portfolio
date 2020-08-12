3f548d53c149b736cd594bc6aa9de7da9f084efa
yeshwanthyk <yeshwanth.yk@gmail.com>
2020-08-12 11:27:07 -0400

    Add Readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Ol' School Portfolio
+
+All of the modern portfolios for showcasing photographic bodies of work seems to be modernised to show images horizontally. This makes them work really well in mobile view. But, this really destroys the carefully planned rhythm of the sequence of images. So, I made this project instead.
+
+A no-frills and highly performant portfolio using [11ty](https://www.11ty.dev/) and [cloudinary](cloudinary.com/)
+
+# Next Steps
+
+- Improve accessibility
+- Dark Mode
--- src/_data/projectImages.js
+++ src/_data/projectImages.js
@@ -1,31 +1,36 @@
 module.exports = {
-  // get the paths of the images based on the title of the
-  // project
-  getPaths(projectName, imagesCount) {
-    const srcsetWidths = [320, 640, 960, 1280];
-    const fallbackWidth = 640;
+	// get the paths of the images based on the title of the
+	// project
+	getPaths(projectName, imagesCount) {
+		const srcsetWidths = [320, 640, 960, 1280];
+		const fallbackWidth = 640;
 
-    let imagesPaths = [];
+		let imagesPaths = [];
 
-    for (i = 1; i < imagesCount + 1; i++) {
-      imagesPaths.push(`${projectName}\/${projectName}-${i}`);
-    }
+		// This would build the images path for how they appear in the folder
+		// The assumption is that the cloudinary folder contains images that are sequenced
+		// the way we want them. And, that they share the same name as the folder they are placed
+		// in
+		for (i = 1; i < imagesCount + 1; i++) {
+			imagesPaths.push(`${projectName}\/${projectName}-${i}`);
+		}
 
-    const finalPaths = [];
-    imagesPaths.forEach((path) => {
-      const fetchBase = `https://res.cloudinary.com/yesh/image/upload/`;
-      const src = `${fetchBase}q_auto,f_auto,w_${fallbackWidth}/${path}.jpg`;
+		// We grab and set the srcset widths for all the images we want to show
+		const finalPaths = [];
+		imagesPaths.forEach(path => {
+			const fetchBase = `https://res.cloudinary.com/yesh/image/upload/`;
+			const src = `${fetchBase}q_auto,f_auto,w_${fallbackWidth}/${path}.jpg`;
 
-      const srcset = srcsetWidths
-        .map((w) => {
-          return `${fetchBase}q_auto:eco,f_auto,w_${w}/${path}.jpg ${w}w`;
-        })
-        .join(', ');
-      finalPaths.push(
-        `<img loading="lazy" src="${src}" srcset="${srcset}" sizes="(max-width: 320px) 300px, 400px" alt="images for project ${projectName}">`
-      );
-    });
+			const srcset = srcsetWidths
+				.map(w => {
+					return `${fetchBase}q_auto:eco,f_auto,w_${w}/${path}.jpg ${w}w`;
+				})
+				.join(", ");
+			finalPaths.push(
+				`<img loading="lazy" src="${src}" srcset="${srcset}" sizes="(max-width: 320px) 300px, 400px" alt="images for project ${projectName}">`
+			);
+		});
 
-    return finalPaths;
-  },
+		return finalPaths;
+	}
 };
